19f04ee2d417879170fb0b0900cccaa900ed5d8c
Santu Ghosh <69458659+me-santu@users.noreply.github.com>
2023-05-05 15:19:56 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -23,6 +23,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You R Banned!.. Contact  [Support](https://t.me/iSmartBoiUjjwal_ib_bot)", quote=True)
+            await cmd.reply_text("You R Banned!.. Contact here [Support](https://t.me/mrpremium_bot)", quote=True)
             return
     await cmd.continue_propagation()
